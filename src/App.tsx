b32e9499573766d87efa49ae4814d3124745071b
Igor Stepanov <stipyk1309@gmail.com>
2024-09-25 00:48:17 +0300

    feat: styled components, GlobalStyles, font & colors, Button & Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
+import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
+
+const H1 = styled.h1`
+	font-size: 30px;
+	font-weight: 600;
+	background-color: rebeccapurple;
+`
+
+const StyledApp = styled.div`
+	background-color: palegreen;
+	padding: 20px;
+`
+
 function App() {
-	return <div>App</div>
+	return (
+		<>
+			<GlobalStyles />
+			<StyledApp>
+				<H1>App</H1>
+				<Input type='text' placeholder='input' />
+				<Button>Check in</Button>
+			</StyledApp>
+		</>
+	)
 }
 export default App
