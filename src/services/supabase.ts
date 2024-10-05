aecb7e9601310a3a6b1e4a472ca11076589c080c
Igor Stepanov <stipyk1309@gmail.com>
2024-10-05 22:23:04 +0300

    chore: env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bcxflhvvngrqocdbdafn.supabase.co'
-const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjeGZsaHZ2bmdycW9jZGJkYWZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc1MTM2ODAsImV4cCI6MjA0MzA4OTY4MH0.w2gi4pOT-MrAQcj8UMD19GXNfrvf636IQMzMXkMqrFc'
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey || '')
 
 export default supabase
