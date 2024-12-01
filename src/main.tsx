a7ce3ccb4c497e8e084e9ed4a4006ce5ba3ebaf3
Igor Stepanov <stipyk1309@gmail.com>
2024-12-01 16:03:35 +0300

    feat: App ErrorBoundary with Error Boundary package

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { StyleSheetManager } from 'styled-components'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorFallback from './ui/ErrorFallback.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<StyleSheetManager shouldForwardProp={() => true}>
-			<App />
+			<ErrorBoundary
+				FallbackComponent={ErrorFallback}
+				onReset={() => window.location.replace('/')}
+			>
+				<App />
+			</ErrorBoundary>
 		</StyleSheetManager>
 	</StrictMode>
 )
